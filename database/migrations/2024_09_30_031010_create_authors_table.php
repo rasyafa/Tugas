5b049e6d8a1335d3edcffa95c2b37e64baa4f9bb
Rasya <rasyarasyifah11@gmail.com>
2024-09-30 10:26:48 +0700

    update file

---
+++ database/migrations/2024_09_30_031010_create_authors_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->id();
+            $table->integer("author_id")->unique();
+            $table->string("author_name");
+            $table->date("date_of_birth");
+            $table->string("place_of_birth");
+            $table->text("biography")->nullable();
+            $table->integer("number_of_books");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('authors');
+    }
+};
